5c12040f9fee6420567cf456dfe5be22ae90c019
JunKoding <wnsrb0407@daum.net>
2022-05-02 19:24:37 +0900

    2022.05.02 - tabBarController 설정 연습

---
+++ tabBarController_tutorial/MyMainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tabBarController_tutorial
+//
+//  Created by JunKyu Lee on 2022/05/02.
+//
+
+import UIKit
+
+class MyMainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
